a052ebd2554acc23b8f2a8c7ed524818e4f878b0
Glenn Gartner <Glenn Gartner>
2016-09-05 13:57:36 -0400

    Correct for new dev environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,5 +85,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Heroku action mailer
-  config.action_mailer.default_url_options = {host: 'https://pacific-beach-10790.herokuapp.com/'}
+  config.action_mailer.default_url_options = {host: 'https://quiet-thicket-54233.herokuapp.com/'}
 end
